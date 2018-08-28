ac2ce626cc180f5d36c22bfa1c322ba7c2f0462c
John Wyles <john@fourstacks.co.uk>
2018-08-28 21:39:06 +0100

    first pass of addField method

--- src/NovaRepeatableFields.php
+++ src/NovaRepeatableFields.php
@@ -13,5 +13,37 @@ class Repeater extends Field
      */
     public $component = 'nova-repeatable-fields';
 
-    
+    public function addField($fieldConfig)
+    {
+        $config = $this->normaliseFieldConfig($fieldConfig);
+
+        $fields = (array_key_exists('sub_fields', $this->meta))
+            ? $this->meta['fields']
+            : [];
+
+        $this->withMeta([
+            'sub_fields' => array_push($fields, $config),
+        ]);
+    }
+
+    private function normaliseFieldConfig($fieldConfig)
+    {
+        $allowedKeys = ['label', 'name', 'placeholder', 'type', 'width'];
+        $config = array_intersect_key($fieldConfig, array_flip($allowedKeys));
+
+        if(! isset($config['name'])){
+            $config['name'] = str_slug($config['name'], '_');
+        }
+        if(! isset($config['placeholder'])){
+            $config['placeholder'] = $config['name'];
+        }
+        if(! isset($config['type'])){
+            $config['type'] = 'text';
+        }
+        if(! isset($config['width'])){
+            $config['width'] = null;
+        }
+
+        return $config;
+    }
 }
